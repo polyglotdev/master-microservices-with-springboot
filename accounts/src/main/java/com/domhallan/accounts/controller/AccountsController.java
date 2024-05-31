f607e1bfb6959225092b778c5cfceb12d4f9eef6
Dom Hallan <dom@domhallan.com>
2024-05-31 12:21:09 -0400

    Refactor: Remove unused endpoints in AccountsController
    The commit removes several redundant endpoints from the AccountsController. The "/hello", "/world", and "/accounts" GET mappings have been deleted as they were not being used, simplifying the controller's structure.

--- accounts/src/main/java/com/domhallan/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/domhallan/accounts/controller/AccountsController.java
@@ -1,27 +1,10 @@
 package com.domhallan.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="/api", produces = "application/json")
 public class AccountsController {
-
-  @GetMapping("/hello")
-  public String Hello() {
-    return "Hello";
-  }
-
-  @GetMapping("/world")
-  public String World() {
-    // Return json object
-    return "{\"message\": \"World\"}";
-  }
-
-  @GetMapping("/accounts")
-  public String Accounts() {
-    // return html
-    return "<h1>Accounts</h1>";
-  }
+  
 }
