c0c13761d6bb0f119e7e42914b01c92cc22a889f
Dom Hallan <dom@domhallan.com>
2024-05-31 12:48:50 -0400

    Added mapping functions in CustomerMapper
    Two new methods have been added to the CustomerMapper class. These methods are responsible for mapping fields between the Customer and CustomerDto objects. The fields that are mapped include name, email, and mobileNumber. This will facilitate data transfer between these two object types.

--- accounts/src/main/java/com/domhallan/accounts/mapper/CustomerMapper.java
+++ accounts/src/main/java/com/domhallan/accounts/mapper/CustomerMapper.java
@@ -5,6 +5,14 @@ import com.domhallan.accounts.entity.Customer;
 
 public class CustomerMapper {
 
+  /**
+   * Maps the fields from a {@link Customer} object to a {@link CustomerDto} object.
+   * The fields that will be mapped are name, email, and mobileNumber.
+   *
+   * @param customer     The Customer object to map from.
+   * @param customerDto  The CustomerDto object to map to.
+   * @return The CustomerDto object with the mapped fields.
+   */
   public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
     customerDto.setName(customer.getName());
     customerDto.setEmail(customer.getEmail());
@@ -12,6 +20,14 @@ public class CustomerMapper {
     return customerDto;
   }
 
+  /**
+   * Maps the fields from a {@link CustomerDto} object to a {@link Customer} object.
+   * The fields that will be mapped are name, email, and mobileNumber.
+   *
+   * @param customerDto The CustomerDto object to map from.
+   * @param customer    The Customer object to map to.
+   * @return The Customer object with the mapped fields.
+   */
   public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
     customer.setName(customerDto.getName());
     customer.setEmail(customerDto.getEmail());
