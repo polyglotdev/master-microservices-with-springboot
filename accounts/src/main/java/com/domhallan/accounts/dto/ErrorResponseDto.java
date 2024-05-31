87a1413f04835e2b37403117b8a7aa763e75d070
Dom Hallan <dom@domhallan.com>
2024-05-31 12:15:44 -0400

    Added ErrorResponseDto for error handling
    Introduced a new DTO, ErrorResponseDto, to handle errors more effectively. This DTO includes fields for the API path where the error occurred, HTTP status code representing the error, an error message and detailed information about the error.

---
+++ accounts/src/main/java/com/domhallan/accounts/dto/ErrorResponseDto.java
@@ -0,0 +1,20 @@
+package com.domhallan.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data @AllArgsConstructor
+public class ErrorResponseDto {
+  // Represents the API path where the error occurred
+  private String apiPath;
+
+  // Represents the error code as a HTTP status
+  private HttpStatus errorCode;
+
+  // Represents the error message
+  private String errorMessage;
+
+  // Represents the error details
+  private String errorDetails;
+}
