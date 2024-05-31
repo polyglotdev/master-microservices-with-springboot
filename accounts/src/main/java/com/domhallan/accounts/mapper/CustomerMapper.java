e7d035afc931c5fc4cd65b8fd449ac35f6443533
Dom Hallan <dom@domhallan.com>
2024-05-31 12:45:07 -0400

    Added CustomerMapper class
    Introduced a new class, CustomerMapper, to handle the conversion between Customer and CustomerDto objects. This includes two static methods: one for mapping from a Customer entity to a CustomerDto object, and another for the reverse operation.

---
+++ accounts/src/main/java/com/domhallan/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,21 @@
+package com.domhallan.accounts.mapper;
+
+import com.domhallan.accounts.dto.CustomerDto;
+import com.domhallan.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+  public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+    customerDto.setName(customer.getName());
+    customerDto.setEmail(customer.getEmail());
+    customerDto.setMobileNumber(customer.getMobileNumber());
+    return customerDto;
+  }
+
+  public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+    customer.setName(customerDto.getName());
+    customer.setEmail(customerDto.getEmail());
+    customer.setMobileNumber(customerDto.getMobileNumber());
+    return customer;
+  }
+}
