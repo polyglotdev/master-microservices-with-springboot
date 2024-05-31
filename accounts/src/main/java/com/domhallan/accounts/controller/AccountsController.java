0b1bd732b5fe01ed864dc98c887262bf68b1062f
Dom Hallan <dom@domhallan.com>
2024-05-31 12:23:01 -0400

    Added account creation endpoint
    A new POST endpoint has been added to the AccountsController. This endpoint, "/create", allows for the creation of a new account by accepting a CustomerDto object in the request body and returning a ResponseDto object with status code 200 and a success message.

--- accounts/src/main/java/com/domhallan/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/domhallan/accounts/controller/AccountsController.java
@@ -1,10 +1,19 @@
 package com.domhallan.accounts.controller;
 
+import com.domhallan.accounts.dto.CustomerDto;
+import com.domhallan.accounts.dto.ResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="/api", produces = "application/json")
 public class AccountsController {
-  
+
+  @PostMapping("/create")
+  public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+    return ResponseEntity.ok(new ResponseDto("200", "Account created successfully"));
+  }
 }
