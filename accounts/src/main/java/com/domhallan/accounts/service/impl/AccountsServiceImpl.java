40c9e7692ed2ed39f801b3e5f93cb490e4598022
Dom Hallan <dom@domhallan.com>
2024-05-31 13:27:55 -0400

    Enhanced account creation process
    Added functionality to create a new account when a customer is created. The new account gets assigned with a random account number and default values for type and branch address. This change simplifies the process of creating an account for a new customer by automating it within the same service method.

--- accounts/src/main/java/com/domhallan/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/domhallan/accounts/service/impl/AccountsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.domhallan.accounts.service.impl;
 
+import com.domhallan.accounts.constants.AccountsConstants;
 import com.domhallan.accounts.dto.CustomerDto;
+import com.domhallan.accounts.entity.Accounts;
 import com.domhallan.accounts.entity.Customer;
 import com.domhallan.accounts.mapper.CustomerMapper;
 import com.domhallan.accounts.repository.AccountsRepository;
@@ -8,6 +10,8 @@ import com.domhallan.accounts.repository.CustomerRepository;
 import com.domhallan.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -17,11 +21,30 @@ public class AccountsServiceImpl implements IAccountsService {
   private CustomerRepository customerRepository;
 
   /**
+   * Creates an account for the given customer.
+   *
    * @param customerDto The customer information for creating the account.
    */
   @Override
   public void createAccount(CustomerDto customerDto) {
     Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
-    customerRepository.save(customer);
+    Customer savedCustomer = customerRepository.save(customer);
+
+    accountsRepository.save(createNewAccount(savedCustomer));
+  }
+
+  /**
+   * @param customer - Customer Object
+   * @return the new account details
+   */
+  private Accounts createNewAccount(Customer customer) {
+    Accounts newAccount = new Accounts();
+    newAccount.setCustomerId(customer.getCustomerId());
+    long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+    newAccount.setAccountNumber(randomAccNumber);
+    newAccount.setAccountType(AccountsConstants.SAVINGS);
+    newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+    return newAccount;
   }
 }
