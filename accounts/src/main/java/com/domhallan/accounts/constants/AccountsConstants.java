62577b5a084ec31ba39d833d4fbd70932e24ba51
Dom Hallan <dom@domhallan.com>
2024-05-31 13:44:01 -0400

    Added new constants for customer validation
    Two new constants have been introduced to handle specific customer-related validations. These include checks for existing customers and duplicate mobile numbers in the database.

--- accounts/src/main/java/com/domhallan/accounts/constants/AccountsConstants.java
+++ accounts/src/main/java/com/domhallan/accounts/constants/AccountsConstants.java
@@ -15,4 +15,7 @@ public final class AccountsConstants {
   public static final String  STATUS_417 = "417";
   public static final String  MESSAGE_417_UPDATE= "Update operation failed. Please try again or contact Dev team";
   public static final String  MESSAGE_417_DELETE= "Delete operation failed. Please try again or contact Dev team";
+  public static final String CUSTOMER_EXISTS = "Customer already exists";
+  public static final String CUSTOMER_MOBILE_NUMBER_ALREADY_EXISTS =
+      "Mobile number already exists in the database";
 }
