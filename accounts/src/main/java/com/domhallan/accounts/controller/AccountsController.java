554bb08fc27f42dec762b5ba958e79ccb882bb80
Dom Hallan <dom@domhallan.com>
2024-05-31 12:26:35 -0400

    Updated HTTP status for account creation
    The HTTP response status code has been updated from 200 to 201 when a new account is successfully created. This change aligns the response with standard RESTful practices, indicating that a resource was successfully created.

--- accounts/src/main/java/com/domhallan/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/domhallan/accounts/controller/AccountsController.java
@@ -2,6 +2,7 @@ package com.domhallan.accounts.controller;
 
 import com.domhallan.accounts.dto.CustomerDto;
 import com.domhallan.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,8 @@ public class AccountsController {
 
   @PostMapping("/create")
   public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
-    return ResponseEntity.ok(new ResponseDto("200", "Account created successfully"));
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(new ResponseDto("201", "Account created successfully"));
   }
 }
