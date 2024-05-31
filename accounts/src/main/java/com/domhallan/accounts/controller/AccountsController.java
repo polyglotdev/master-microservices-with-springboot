7a06f7820cff7d73bb6ee9cb00b79e64fdb68e96
Dom Hallan <dom@domhallan.com>
2024-05-31 12:31:44 -0400

    Updated AccountsController for better maintainability
    Enhanced the AccountsController by importing and utilizing constants from the AccountsConstants class. This improves code readability and maintainability. Also, updated the @RequestMapping annotation to use MediaType.APPLICATION_JSON_VALUE instead of a hardcoded string, enhancing consistency across the application.

--- accounts/src/main/java/com/domhallan/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/domhallan/accounts/controller/AccountsController.java
@@ -1,8 +1,10 @@
 package com.domhallan.accounts.controller;
 
+import com.domhallan.accounts.constants.AccountsConstants;
 import com.domhallan.accounts.dto.CustomerDto;
 import com.domhallan.accounts.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path="/api", produces = "application/json")
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
 
   @PostMapping("/create")
   public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
     return ResponseEntity
         .status(HttpStatus.CREATED)
-        .body(new ResponseDto("201", "Account created successfully"));
+        .body(new ResponseDto(AccountsConstants.STATUS_201,
+            AccountsConstants.MESSAGE_201));
   }
 }
